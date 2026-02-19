9dc143c6cf2eacba53020eddc3465e02c5635431
Mohamed Tarek <m.tarek@moveitgate.com>
2026-02-19 15:33:40 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.teal,
+          brightness: Brightness.dark,
+        ),
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter Map'),
+          centerTitle: true,
+          backgroundColor: Colors.teal,
+        ),
+        bottomNavigationBar: NavigationDestination(
+          label: 'home',
+          icon: Icon(Icons.home),
+        ),
+      ),
+    );
+  }
+}
